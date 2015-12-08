fbf6cb9cbcaa9e82b97903734cf629e3440d77fc
Ty-Yamaguchi <Ty@KITT.local>
2015-12-07 15:12:02 -1000

    Added a missing }

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -42,7 +42,8 @@ if (Meteor.isClient) {
 	Template.task.events({
     "click .toggle-checked": function () {
       // Set the checked property to the opposite of its current value
-    Meteor.call("setChecked", this._id, ! this.checked);,
+    Meteor.call("setChecked", this._id, ! this.checked);
+    },
     "click .delete": function () {
       Meteor.call("deleteTask", this._id);
     }
