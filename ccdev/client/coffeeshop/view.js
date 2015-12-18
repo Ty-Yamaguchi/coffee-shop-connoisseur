9dba8f38df7692b64efb31b9bee6ec67dcb550c3
matinglis <mat@zendy.net>
2015-12-18 10:49:56 -1000

    Added Google Maps to Coffeeshop View Template

--- ccdev/client/coffeeshop/view.js
+++ ccdev/client/coffeeshop/view.js
@@ -1,5 +1,28 @@
-//Coffeeshop List View Helper
-  Template.coffeeshopList.helpers({
-    Cafes: function () {
-		return Cafes.find({}, {sort: {createdAt: -1}});    }
+// Google Maps
+  Template.coffeeshopView.onRendered(function() {
+	GoogleMaps.load();
+  });
+
+  Template.coffeeshopView.helpers({
+    coffeeshopMapOptions: function() {
+      // Make sure the maps API has loaded
+      if (GoogleMaps.loaded()) {
+        // Map initialization options
+        return {
+          center: new google.maps.LatLng(22.084494, -144.9631),
+          zoom: 8
+        };
+      }
+    }
+  });
+
+  Template.coffeeshopView.onCreated(function() {
+    // We can use the `ready` callback to interact with the map API once the map is ready.
+    GoogleMaps.ready('coffeeshopMap', function(map) {
+      // Add a marker to the map once it's ready
+      var marker = new google.maps.Marker({
+        position: map.options.center,
+        map: map.instance
+      });
+    });
   });
