e6d144cf6ce4d67f4da1a827beaffa7f9d975135
Ty-Yamaguchi <Ty@KITT.local>
2015-12-17 17:08:32 -1000

    Removed check packages from project

--- ccdev/lib/methods/coffeeshop.js
+++ ccdev/lib/methods/coffeeshop.js
@@ -3,26 +3,22 @@
 Meteor.methods({
 	addCafe : function (cafeAttributes) {
 		// Only continue if user is logged in
-		check(Meteor.userId(), String);
-		check(cafeAttributes, {
-			name: String,
-			placeid: String,
-			description: String,
-			csimage: String
-		});
+		if (! Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}	
 		var user = Meteor.user();
 		var cafe = _.extend(cafeAttributes, {
 			owner: user._id,
 			username: user.username,
 			createdAt: new Date()
 		});
+
 		// Insert a cafe into the collection
 		var cafeId = Cafes.insert(cafe);
 		return {
 			_id: cafeId
     	};
-	},
-	   
+	},   
 	removeCafe : function (id) {
 		if (! Meteor.userId()) {
 			throw new Meteor.Error("not-authorized");
