6e55d8da90323edd4bd4d2f843568f53b941d646
Ty-Yamaguchi <Ty@KITT.local>
2015-12-18 10:24:09 -1000

    Preload Coffeeshops into database if none exist

---
+++ ccdev/server/fixtures.js
@@ -0,0 +1,52 @@
+if (Cafes.find().count() === 0) {
+  Cafes.insert({
+	  name: 'Little Fish Coffee Co.',
+	  placeid: 'ChIJfSJxVGUGB3wRU-rWruQw_do',
+	  streetAddress: '3900 Hanapepe Rd',
+	  city: 'Hanapepe',
+	  state: 'Hawaii',
+	  zip: '96716',
+	  description: 'Little Fish coffee offer great espresso and pastries. The atmosphere is airy and charming. Make sure you have time to sit down and sip up because there’s a gem of a garden, a cool oasis on a typical hot West Side day, hidden in the back of the store. Enjoy!',
+	  csimage: '/img/little-fish.jpg'
+  });
+  Cafes.insert({
+	  name: 'Kalaheo Cafe & Coffee Company',
+	  placeid: 'ChIJfzUUJfgEB3wR994OrXg7kEY',
+	  streetAddress: '2-2560 Kaumualii Hwy',
+	  city: 'Kalaheo',
+	  state: 'Hawaii',
+	  zip: '96741',
+	  description: 'Whenever we’re heading through Kalaheo, we try to make the time for a stop at Kalaheo Cafe.  The food is great and the atmosphere is relaxed.',
+	  csimage: '/img/kalaheo-cafe.jpg'
+  });
+  Cafes.insert({
+	  name: 'Kauai Coffee Company',
+	  placeid: 'ChIJURsfO8MFB3wReNXq-qP5yaU',
+	  streetAddress: '870 Halewili Rd',
+	  city: 'Kalaheo',
+	  state: 'Hawaii',
+	  zip: '96741',
+	  description: 'Kauai coffee is one of our favorite tourist day activities.  First of all – it’s FREE!  Second, there’s COFFEE!  Can you sense the caffeine buzz?  Located on the south side of Kauai in Kalaheo, Kauai Coffee is the perfect stop on the way to Kokee.  There must be at least fifteen coffee types to sample – and they are great!',
+	  csimage: '/img/coffee-fruit.jpg'
+  });
+  Cafes.insert({
+	  name: 'Small Town Coffee Co.',
+	  placeid: 'ChIJNeHCRvLgBnwRrIKpQGUFsIg',
+	  streetAddress: '4-1543 Kuhio Hwy',
+	  city: 'Kapaa',
+	  state: 'Hawaii',
+	  zip: '96746',
+	  description: 'Small Town’s atmosphere begs you to slow down, kick back, and sip on a cup of joe. It’s a great little coffee shop hidden in the Kauai craft fair at the north end of Kapaa town. Small Town’s coffee is consistently the most caffeinated cup on Kauai. Beware (and enjoy!)\n*Update – the coffee shop has relocated to a small bus not far from its original location.  Still good coffee – not quite the same atmosphere.  We recommend getting the coffee and walking across the street and drinking it at the beach ',
+	  csimage: '/img/small-town.jpg'
+  });
+  Cafes.insert({
+	  name: 'Art Cafe Hemingway',
+	  placeid: 'ChIJabHFQ_LgBnwROHHyufv_3m4',
+	  streetAddress: '4-1495 Kuhio Hwy',
+	  city: 'Kapaa',
+	  state: 'Hawaii',
+	  zip: '96746',
+	  description: 'Cafe Hemingway is a savory piece of Europe, tucked on the north end of Kapaa town. I go there all time; so often that I don’t want to think about it too much because I don’t want to know how much I spend there every month! The atmosphere is classy and the service is relaxed, which is another way to say that you shouldn’t go there if you’re in a hurry. Grab a seat, relax, and enjoy the food prepared and drinks concocted by Markus and Jana, the culinary masterminds who own the place.',
+	  csimage: '/img/espresso.jpg'
+  });
+}
