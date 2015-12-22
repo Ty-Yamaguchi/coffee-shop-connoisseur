8c57814f067c6f73e1f8127e59123270bb11c06e
Ty-Yamaguchi <Ty@KITT.local>
2015-12-21 15:30:10 -1000

    Grab coffeeshop details dynamically

--- ccdev/client/coffeeshop/view.js
+++ ccdev/client/coffeeshop/view.js
@@ -1,30 +1,66 @@
 // Google Maps
 Template.coffeeshopView.onRendered(function() {
-	GoogleMaps.load({ v: '3', key: 'AIzaSyDuz95a5e7907yAlgPt3WkGgbhDP6V-RA4'});
+	GoogleMaps.load({ v: '3', key: 'AIzaSyDuz95a5e7907yAlgPt3WkGgbhDP6V-RA4', libraries: 'places'});
 });
 
-  Template.coffeeshopView.helpers({
-    coffeeshopMapOptions: function() {
-    	var lat = this.lat;
-    	var lng = this.lng;
-      // Make sure the maps API has loaded
-      if (GoogleMaps.loaded()) {
-        // Map initialization options
-        return {
-          center: new google.maps.LatLng(lat, lng),
-          zoom: 16
-        };
-      }
-    }
-  });
+Template.coffeeshopView.helpers({
+	coffeeshopMapOptions: function() {
+		var lat = this.lat;
+		var lng = this.lng;
+	  // Make sure the maps API has loadeds
+	  if (GoogleMaps.loaded()) {
+	    // Map initialization options
+	    return {
+	      center: new google.maps.LatLng(lat, lng),
+	      zoom: 16
+	    };
+	  }
+	},
+	coffeeshopAddress : function() {
+		return Session.get("coffeeshopAddress");
+	},
+	coffeeshopOpen : function() {
+		var open = Session.get("coffeeshopOpen");
+		if (open) {
+			return "Open";
+		} else {
+			return "Closed";
+		}
+	},
+	coffeeshopPhone : function() {
+		return Session.get("coffeeshopPhone");
+	},
+	coffeeshopHours : function(day) {
+		var hours = Session.get("coffeeshopHours");
+		return hours[day];
+	}
+});
 
 Template.coffeeshopView.onCreated(function() {
+	var placeid = this.data.cafe.placeid;
 	// We can use the `ready` callback to interact with the map API once the map is ready.
 	GoogleMaps.ready('coffeeshopMap', function(map) {
 			// Add a marker to the map once it's ready
 			var marker = new google.maps.Marker({
 			position: map.options.center,
 			map: map.instance
-		});
+			});
+
+			var service = new google.maps.places.PlacesService(map.instance);
+			
+			// We will request coffeeshop details using the placeId
+			var request = {
+				placeId: placeid
+			};
+			
+			// Get details about the coffeeshop and place them in Session variables
+			service.getDetails(request, function(place, status) {
+			  if (status == google.maps.places.PlacesServiceStatus.OK) {
+			    Session.set("coffeeshopAddress", place.formatted_address);
+			    Session.set("coffeeshopOpen", place.opening_hours.open_now);
+			    Session.set("coffeeshopPhone", place.formatted_phone_number);
+			    Session.set("coffeeshopHours", place.opening_hours.weekday_text);
+			  }
+			});
 	});
 });
